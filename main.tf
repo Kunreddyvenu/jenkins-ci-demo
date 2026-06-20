b6dfc5abd3d573cd4346ffb6a74e241f1bc3ab6f
Kunreddyvenu <kunreddyvenu88@gmail.com>
2026-06-20 06:18:34 +0530

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "devops-practice-server" {
   ami           = "0220d79f3f480ecf5"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "jenkins-terraform-practice"
